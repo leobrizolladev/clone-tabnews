7b932274d5aa3a443dbb71835f14f54cb380b8aa
Leonardo Brizolla <leonardo.brizolla@compasso.com.br>
2025-04-11 16:22:37 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(req, res) {
-  res.status(200).json({
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({
     status: "ok",
     message: "API está funcionando",
   });
